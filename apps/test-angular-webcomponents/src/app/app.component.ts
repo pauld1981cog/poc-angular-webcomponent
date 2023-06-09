b4433fa5fe5451944e81f62acc036fd765d609ec
Paul <357787@cognizant.com>
2023-06-09 16:49:49 +0530

    Initial commit

---
+++ apps/test-angular-webcomponents/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poc-angular-webcomponents-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-angular-webcomponents';
+}
